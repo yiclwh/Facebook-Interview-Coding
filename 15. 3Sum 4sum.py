7e1b59061a33b7902a5b4c5b36b3411f9ac4184f
yiclwh <yiclwh@gmail.com>
2018-06-08 15:54:04 -0700

    Update and rename 15. 3Sum.py to 15. 3Sum 4sum.py

--- 15. 3Sum 4sum.py
+++ 15. 3Sum 4sum.py
@@ -75,4 +75,65 @@ class Solution(object):
                     elif n<inverse< p or inverse == 0:
                         rst.append([n, inverse, p])
         return rst
+
+# 4sum
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if not nums or len(nums) < 4:
+            return []
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                k, l = j + 1, len(nums)-1
+                while k< l:
+                    s = nums[i] + nums[j] + nums[k] + nums[l]
+                    if s == target:
+                        res.append([nums[i], nums[j], nums[k], nums[l]])
+                        while k < l and nums[k] == nums[k+1]:
+                            k += 1
+                        k += 1
+                        while l > k and nums[l] == nums[l-1]:
+                            l-=1
+                        l-= 1
+                    elif s < target:
+                        k += 1
+                    else: # > target
+                         l -= 1
+        return res
+    
+    # 一个最优解
+    def fourSum(self, nums, target):
+        def findNsum(nums, target, N, result, results):
+            if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:  # early termination
+                return
+            if N == 2: # two pointers solve sorted 2-sum problem
+                l,r = 0,len(nums)-1
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # recursively reduce N
+                for i in range(len(nums)-N+1):
+                    if i == 0 or (i > 0 and nums[i-1] != nums[i]):
+                        findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+    results = []
+    findNsum(sorted(nums), target, 4, [], results)
+    return results
                     
