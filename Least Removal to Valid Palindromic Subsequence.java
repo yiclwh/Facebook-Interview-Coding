885a4732355f2879aa4ea6a2434cfcd46f48daf5
yiclwh <yiclwh@gmail.com>
2018-06-26 15:47:34 -0700

    Update Least Removal to Valid Palindromic Subsequence.java

--- Least Removal to Valid Palindromic Subsequence.java
+++ Least Removal to Valid Palindromic Subsequence.java
@@ -6,6 +6,26 @@ Least Removal to Valid Palindromic Subsequence
 其中一个方法是求出当前string和它的reversed string的LCS，然后用当前s.length()减去LCS长度就得出最少删除/插入字符的回文了
 你可以试试看，我这个方法是通用的，比如输入abca，它会输出1而不会是3，若输出3那就成了只解决删头尾情况了
 
+def minDeletePal(s):
+    if not s:
+        return 0
+    m = findLcs(s, s[::-1])
+    return len(s) - m
+
+def findLcs(a, b):
+    l1, l2 = len(a), len(b)
+    dp = [[0 for i in range(l1+1)] for j in range(l2+1)]
+    for i in range(1, l1+1):
+        for j in range(1, l2+1):
+            if a[i-1] == b[j-1]:
+                dp[j][i] = dp[j-1][i-1] + 1
+            else:
+                dp[j][i] = max(dp[j][i-1], dp[j-1][i])
+    return dp[l2][l1]
+
+print('Input \'abca\' Expected result 1, got result ', minDeletePal('abca'))
+print('Input \'abcba\' Expected result 0, got result ', minDeletePal('abcba'))
+
 LCS: [YouTube] // https://www.youtube.com/watch?v=NnD96abizww
 
 public int minRemovalPalindrome(String s) {
